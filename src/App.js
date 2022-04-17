c03cfa69b970873563f47fe22bcb9661828e4474
Parsa Zarabian <zarabiparsa@gmail.com>
2022-04-17 13:06:53 +0430

    The base store was created.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
+//components
+import Store from './components/Store';
 
+
+//context
 import ProductContextProvider from './context/ProductContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
-      
+      <Store />
     </ProductContextProvider>
   );
 }
